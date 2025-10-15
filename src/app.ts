58d66a646f000879bf647440e41ff23aa3a04fe0
vipshae <vipul212@gmail.com>
2025-10-14 23:57:14 -0400

    update route path

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// app.use('/api/generate', generateRoute(wordList));
 app.use('/api/generate', generateRoute(wordList));
 
 app.listen(PORT, () => {
